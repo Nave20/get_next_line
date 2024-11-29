fb283e0d8db6114dde953ccabc1cf4dbb216a2a6
28 <vpirotti@z4r8p2.42lyon.fr>
2024-11-29 15:36:09 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpirotti <vpirotti@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 14:00:22 by vpirotti          #+#    #+#             */
+/*   Updated: 2024/11/29 14:03:34 by vpirotti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+#include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
